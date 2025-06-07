233ee49b617f90e93ef6674cbd1343e8d2ab10dd
Shimul Sarker <shimul128815@gmail.com>
2025-06-08 00:58:19 +0600

    Started my part

--- Python/shimul.py
+++ Python/shimul.py
@@ -4,9 +4,11 @@ print ("Hello World!")
 
 #Output: Hello World!
 
+
 #Variables in Python, it has no command for declaring a variable.
 x = 4
 y = 'shimul'
-
 print(x)
 print(y)
+#Output: 4,shimul
+
