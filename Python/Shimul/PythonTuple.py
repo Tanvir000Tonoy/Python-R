4130e0f5494e3e763577f96a9912962bf8dbda5c
Shimul Sarker <shimul128815@gmail.com>
2025-07-04 23:49:24 +0600

    Update the PythonTuple file

--- Python/Shimul/PythonTuple.py
+++ Python/Shimul/PythonTuple.py
@@ -1,3 +1,11 @@
 #Tuple
 mytuple = ("apple", "banana", "cherry")
 print(mytuple)
+
+#Print the number of items in the tuple by using len() function
+mytuple=('shimul','tanvir','bandhan')
+print(len(mytuple))
+
+#Access tuple items
+mytuple2=(1,4,6,8,9)
+print(mytuple2[2])
