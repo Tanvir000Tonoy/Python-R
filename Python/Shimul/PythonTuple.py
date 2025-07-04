439c3fc220f6f8bf2688403ded60d8fc0169c48a
Shimul Sarker <shimul128815@gmail.com>
2025-07-04 23:57:58 +0600

    No more today

--- Python/Shimul/PythonTuple.py
+++ Python/Shimul/PythonTuple.py
@@ -9,3 +9,11 @@ print(len(mytuple))
 #Access tuple items
 mytuple2=(1,4,6,8,9)
 print(mytuple2[2])
+
+#Range of indexes
+mytuple=('Habul','Dabul','Kabul','Kamal','Jamal','Romal')
+print(mytuple[1:4])
+
+#To determine if a specified item is present in a tuple use the in keyword
+if 'Habul' in mytuple:
+    print('yes')
